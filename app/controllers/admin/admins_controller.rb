4a0b0f373ade57ca8e21866c47ba64136846c0d1
Yuki-K <s143068@gmail.com>
2020-10-24 11:03:23 +0900

    finish fixing design

--- app/controllers/admin/admins_controller.rb
+++ app/controllers/admin/admins_controller.rb
@@ -4,26 +4,27 @@ class Admin::AdminsController < ApplicationController
   end
 
   def search
-    keyword = search_params[:keyword]
-    unless keyword.blank?
-      if Customer.by_last_name(keyword).any?  
-        @customers = Customer.by_last_name(keyword).page(params[:page])
-        flash.now[:notice] = "Found #{ @customers.count } #{ "person".pluralize(@customers.count)}"
-        render "admin/customers/index"
-      elsif Customer.by_first_name(keyword).any?
-        @customers = Customer.by_first_name(keyword).page(params[:page])
-        flash.now[:notice] = "Found #{ @customers.count } #{ "person".pluralize(@customers.count)}"
-        render "admin/customers/index"
-      elsif Item.by_name(keyword).any?
-        @items = Item.by_name(keyword).page(params[:page])
-        flash.now[:notice] = "Found #{ @items.count } #{ "item".pluralize(@items.count) }"
-        render "admin/items/index"
+      keyword = search_params[:keyword]
+      unless keyword.blank?
+        if Customer.by_last_name(keyword).any?  
+          @customers = Customer.by_last_name(keyword).page(params[:page])
+          flash.now[:notice] = "Found #{ @customers.count } #{ "person".pluralize(@customers.count)}"
+          render "admin/customers/index"
+        elsif Customer.by_first_name(keyword).any?
+          @customers = Customer.by_first_name(keyword).page(params[:page])
+          flash.now[:notice] = "Found #{ @customers.count } #{ "person".pluralize(@customers.count)}"
+          render "admin/customers/index"
+        elsif Item.by_name(keyword).any?
+          @items = Item.by_name(keyword).page(params[:page])
+          flash.now[:notice] = "Found #{ @items.count } #{ "item".pluralize(@items.count) }"
+          render "admin/items/index"
+        else
+          redirect_to request.referer, notice: "Nothing found"
+        end
       else
-        redirect_to request.referer, notice: "Nothing found"
+        redirect_to request.referer, notice: "キーワードが必要です。"
       end
-    else
-      redirect_to request.referer, notice: "キーワードが必要です。"
-    end
+    debugger
   end
 
     private
